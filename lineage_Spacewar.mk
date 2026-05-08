b28566ba4adc14a0ad6b23c96d620fcb75179d9d
adityayyy <a.ganeshgudi11@gmail.com>
2026-05-08 15:03:12 +0200

    build is UnOfficial

--- lineage_Spacewar.mk
+++ lineage_Spacewar.mk
@@ -16,7 +16,7 @@ $(call inherit-product, device/nothing/Spacewar/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Lunaris flags
-LUNARIS_BUILD_TYPE := OFFICIAL
+LUNARIS_BUILD_TYPE := UNOFFICIAL
 WITH_GMS := true
 TARGET_CUSTOM_UDFPS := true
 TARGET_OPTIMIZED_DEXOPT := true
